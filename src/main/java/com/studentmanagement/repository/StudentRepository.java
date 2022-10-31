62ca4201c9d34f976e344b738e797aa2055c5363
Uzairzaheer03 <115987328+Uzairzaheer03@users.noreply.github.com>
2022-10-31 09:01:22 -0700

    Student Management System

--- src/main/java/com/studentmanagement/repository/StudentRepository.java
+++ src/main/java/com/studentmanagement/repository/StudentRepository.java
@@ -3,5 +3,10 @@ package com.studentmanagement.repository;
 import com.studentmanagement.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+
+    Optional<Student> findByCity(String city);
 }
