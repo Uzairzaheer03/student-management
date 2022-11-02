3b3befc42ba485936c6c61bd1c7c9f6aa9b2c136
Uzairzaheer03 <115987328+Uzairzaheer03@users.noreply.github.com>
2022-11-02 01:13:56 -0700

    Student Management System

--- src/main/java/com/studentmanagement/service/StudentService.java
+++ src/main/java/com/studentmanagement/service/StudentService.java
@@ -3,6 +3,7 @@ package com.studentmanagement.service;
 import com.studentmanagement.domain.Student;
 import com.studentmanagement.dto.StudentDto;
 import com.studentmanagement.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class StudentService {
        private final StudentRepository studentRepository;
 
@@ -24,11 +26,12 @@ public class StudentService {
        public String createStudent(StudentDto studentDto){
               Student student = new Student(studentDto.getName(),studentDto.getEmail(),studentDto.getPhone(),studentDto.getCity());
               studentRepository.save(student);
+              log.info("saved student{}",student);
               return " saved student";
        }
 
        public Optional<Student> getStudent(int id) {
-
+              log.info("get student{}",id);
               return studentRepository.findById(id);
        }
 
@@ -41,6 +44,7 @@ public class StudentService {
                      optionalStudent.get().setPhone(studentDto.getPhone());
                      optionalStudent.get().setCity(studentDto.getCity());
                      studentRepository.save(optionalStudent.get());
+                     log.info("update student{}",studentDto);
                      return " update student ";
               }else{
                      return "student not exist with id:"+id;
@@ -50,10 +54,12 @@ public class StudentService {
 
        public String deleteStudent(int id) {
               studentRepository.deleteById(id);
+              log.info("delete student",id);
               return " delete student";
        }
 
        public List<Student> getAllStudent() {
+              log.info("findAll{}"  );
               return new ArrayList<>(studentRepository.findAll());
        }
 
@@ -61,14 +67,13 @@ public class StudentService {
        public  Page<Student> getAllStudent(Integer pageNumber, Integer pageSize) {
 
               Pageable pageable = PageRequest.of(pageNumber, pageSize);
-
-
+              log.info("findAll{}",pageable);
               return this.studentRepository.findAll(pageable);
        }
 
 
        public List<Student> getStudentByCity(String city) {
-
+              log.info("findByCity{}",city);
               return studentRepository.findByCity(city);
        }
 }
